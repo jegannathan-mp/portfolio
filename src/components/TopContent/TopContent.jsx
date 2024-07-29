55005add1af164d6d445c41c196d68e7845c5cdc
jegannathan-mp <mpjegannathanmech@gmail.com>
2024-07-29 23:39:23 +0530

    ready to deploy

--- src/components/TopContent/TopContent.jsx
+++ src/components/TopContent/TopContent.jsx
@@ -9,7 +9,7 @@ const TopContent = () => {
         <div className='topContent_container'>
             <h1>Mr.Jegan Nathan M P</h1>
             <p>A professional Web and App Developer</p>
-            <a href="https://drive.google.com/file/d/1sL98PyOs3A4m-GBQRaEDKOo6pGWcCLZ8/view?usp=drivesdk" target='_blank' rel="noopener noreferrer" >
+            <a href="https://drive.google.com/file/d/1tjg8KpC98W8uKpRvaBJzcmh37at0DG4U/view?usp=drivesdk" target='_blank' rel="noopener noreferrer" >
                 <button className='topContent_downloadButton'>Download CV</button>
             </a>
             <Link to='projects' smooth={true} duration={500} >
